8c5640c10a31769e82d2464ff8746bb919ae48f6
Llovet Nicholas <nllovet20199@columbushs.com>
2020-02-12 07:38:49 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+// All the variables that activate the program
+
+let
+
+let passCheck = document.getElementById('go');
+let passScramble = document.getElementById('scramble');
+let passKey = document.getElementById('goMake');
